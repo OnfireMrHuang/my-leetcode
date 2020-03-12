6fd4139f9e164bcd2909c4c973dbb330d48fdb38
OnfireMrHuang <engineer_mrhuang@163.com>
2020-03-12 20:11:01 +0800

    添加例子

---
+++ exercise/function/kethlargest/kethlargest.go
@@ -0,0 +1,20 @@
+package kethlargest
+
+import (
+	"container/heap"
+)
+
+type KthLargest struct {
+	
+}
+
+
+func Constructor(k int, nums []int) KthLargest {
+
+
+}
+
+
+func (this *KthLargest) Add(val int) int {
+
+}
---
+++ exercise/test/kethlargest_test.go
@@ -0,0 +1,13 @@
+package test
+
+import (
+	"testing"
+	"exercise/function/kethlargest"
+)
+
+func TestKethLargest(t *testing.T)  {
+
+	k := kethlargest.Constructor()
+
+	
+}
