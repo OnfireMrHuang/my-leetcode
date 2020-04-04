69deee1511c9cb8d249fd32c5dd5823d004338a8
huangwenwu <engineer_mrhuang@163.com>
2020-04-05 02:43:04 +0800

    提交动态规划空代码，这块还要强化

--- 300.最长上升子序列.go
+++ 300.最长上升子序列.go
@@ -4,9 +4,21 @@
  * [300] 最长上升子序列
  */
 
+import "sort"
+
 // @lc code=start
 func lengthOfLIS(nums []int) int {
+	dp := []int{}
 
+	for _,num := range nums {
+		i := sort.SearchInts(dp,num)
+		if i == len(dp) {
+			dp = append(dp,num)
+		} else {
+			dp[i] = num
+		}
+	}
+	return len(dp)
 }
 // @lc code=end
 
